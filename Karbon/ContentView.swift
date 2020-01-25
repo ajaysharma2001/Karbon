a445d51c490e9b098670b9900ca025afa2a71bcf
Ajay Sharma <ajayks@msn.com>
2020-01-25 15:16:06 -0500

    first
    test

--- Karbon/ContentView.swift
+++ Karbon/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            TabView(selection: /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                Text("Tab Content 1").tabItem { Text("Tab Label 1") }.tag(1)
+                /*@START_MENU_TOKEN@*/Text("Tab Content 2").tabItem { Text("Tab Label 2") }.tag(2)/*@END_MENU_TOKEN@*/
+                Text("Tab Content 3").tabItem { Text("Tab Label 3") }.tag(3)
+            }
+        }
     }
 }
 
