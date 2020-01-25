2d9fc12dd19e9adcc8acc7786f66e77434174499
Ajay Sharma <ajayks@msn.com>
2020-01-25 16:46:18 -0500

    home stuff

--- Karbon/HomeView.swift
+++ Karbon/HomeView.swift
@@ -10,7 +10,42 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Circle()
+                    .strokeBorder(lineWidth: 20, antialiased: false)
+                    .foregroundColor(Color.red)
+                    .padding(.vertical, 10)
+                Circle()
+                    .strokeBorder(lineWidth: 20, antialiased: false)
+                    .foregroundColor(Color.green)
+                    .padding(35)
+                    
+                VStack {
+                    Text("CO2 Footprint")
+                        .foregroundColor(Color.red)
+                    Text("Points")
+                        .foregroundColor(Color.green)
+                }
+            
+            }
+
+            List {
+                Section(header: Text("Break Down")) {
+                    HStack {
+                        Text("Driving")
+                        RoundedRectangle(cornerRadius: 10, style: .circular)
+                            .padding(.vertical)
+                    }
+                    HStack {
+                        Text("Food")
+                        RoundedRectangle(cornerRadius: 10, style: .circular)
+                            .padding(.vertical)
+                            .padding(.leading, 14)
+                    }
+                }
+            }.environment(\.defaultMinListRowHeight, 100)
+        }
     }
 }
 
