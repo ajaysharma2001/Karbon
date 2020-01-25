13292e46fa850356cd72386d9802d72163838125
Ajay Sharma <ajayks@msn.com>
2020-01-25 18:22:25 -0500

    added ring animation

---
+++ Karbon/Arc.swift
@@ -0,0 +1,22 @@
+//
+//  Arc.swift
+//  Karbon
+//
+//  Created by Ajay Sharma on 2020-01-25.
+//  Copyright © 2020 MrGoose. All rights reserved.
+//
+
+import SwiftUI
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        
+        return path
+    }
+}
--- Karbon/HomeView.swift
+++ Karbon/HomeView.swift
@@ -6,21 +6,62 @@
 //  Copyright © 2020 MrGoose. All rights reserved.
 //
 
+    /*
+    Arc(startAngle: .degrees(270), endAngle: .degrees(80), clockwise: false)
+        .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        .frame(width: 300, height: 300)
+    Arc(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: false)
+        .stroke(Color.red.opacity(0.1), lineWidth: 10)
+        .frame(width: 300, height: 300)
+        
+    
+    Arc(startAngle: .degrees(270), endAngle: .degrees(200), clockwise: false)
+        .stroke(Color.green, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        .frame(width: 270, height: 270)
+    Arc(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: false)
+        .stroke(Color.green.opacity(0.1), lineWidth: 10)
+    .frame(width: 270, height: 270)
+        
+*/
+
 import SwiftUI
 
 struct HomeView: View {
+    @State var drawRed = false
+    @State var drawGreen = false
+    
     var body: some View {
         VStack {
             ZStack {
                 Circle()
-                    .strokeBorder(lineWidth: 20, antialiased: false)
-                    .foregroundColor(Color.red)
-                    .padding(.vertical, 10)
+                    .trim(from: 0, to: drawRed ? 1/2 : 0)
+                    .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                    .animation(Animation.easeOut(duration: 2.5))
+                    .frame(width: 300, height: 300)
+                    .rotationEffect(.degrees(270))
+                    .onAppear() {
+                        self.drawRed.toggle()
+                    }
                 Circle()
-                    .strokeBorder(lineWidth: 20, antialiased: false)
-                    .foregroundColor(Color.green)
-                    .padding(35)
-                    
+                    .trim(from: 1, to: 1)
+                    .stroke(Color.red.opacity(0.1), style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                    .frame(width: 300, height: 300)
+
+                
+                Circle()
+                    .trim(from: 0, to: drawGreen ? 2/3 : 0)
+                    .stroke(Color.green, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                    .animation(Animation.easeOut(duration: 2.5))
+                    .rotationEffect(.degrees(270))
+                    .frame(width: 270, height: 270)
+                    .onAppear() {
+                        self.drawGreen.toggle()
+                    }
+                Circle()
+                    .trim(from: 1, to: 1)
+                    .stroke(Color.green.opacity(0.1), style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                    .frame(width: 270, height: 270)
+                
                 VStack {
                     Text("CO2 Footprint")
                         .foregroundColor(Color.red)
@@ -28,7 +69,7 @@ struct HomeView: View {
                         .foregroundColor(Color.green)
                 }
             
-            }
+            }.padding(.vertical)
 
             List {
                 Section(header: Text("Break Down")) {
